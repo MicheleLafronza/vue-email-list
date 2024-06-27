926460d3546617be1ce618ef83f6653b1e4d0eb3
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-27 15:53:56 +0200

    feat: add axios get random mail API list

--- js/main.js
+++ js/main.js
@@ -4,13 +4,20 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        EmailLists: [],
       }
     },
     methods: {
 
     },
     mounted() {
-        console.log(`the component is now mounted.`)
-      }
+        for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((randomEmail) => {
+                    const mail = randomEmail.data.response;
+                    this.EmailLists.push(mail);
+            })
+        }
+        console.log(this.EmailLists);
+    }
   }).mount('#app')
